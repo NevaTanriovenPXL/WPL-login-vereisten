a79bb2bb0761b0238e9eb8cd6661f9bfef5232d3
Neva <neva.tanrioven@student.pxl.be>
2025-11-06 13:20:43 +0100

    DEMO-02

--- loginVereisten/loginVereisten/Program.cs
+++ loginVereisten/loginVereisten/Program.cs
@@ -5,12 +5,14 @@
         static void Main(string[] args)
         {
             string username;
-            
+            string wachtwoord;
+
 
             Console.Write("Gebruikersnaam: ");
             username = Console.ReadLine();
 
-            
+            Console.Write("Wachtwoord: ");
+            wachtwoord = Console.ReadLine();
         }
     }
 }
